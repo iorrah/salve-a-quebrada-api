135be4e89f30d6da51a702dfc512fdc2920821dd
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-03 17:43:47 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// defining the Express app
+const app = express();
+
+// defining an array to work as the database (temporary solution)
+const stores = [
+  {title: 'Hello, world (again)!'}
+];
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+// defining an endpoint to return all stores
+app.get('/', (req, res) => {
+  res.send(stores);
+});
+
+// starting the server
+app.listen(8080, () => {
+  console.log('listening on port 8080');
+});
