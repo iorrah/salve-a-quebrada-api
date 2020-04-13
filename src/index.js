8139e5104367fd159239dfaf844f0bdbbc251c78
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-13 15:44:09 +0200

    Fix POST request

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const createError = require('http-errors');
+const path = require('path');
+
 const store = require('./store');
 
 // defining the Express app
@@ -15,7 +18,10 @@ const API_STORE = '/v1/stores';
 app.use(helmet());
 
 // using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // enabling CORS for all requests
 app.use(cors());
@@ -23,11 +29,34 @@ app.use(cors());
 // adding morgan to log HTTP requests
 app.use(morgan('combined'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // defining an endpoint to return all stores
 app.get(API_STORE, store.get);
 
 // defining an endpoint to add a new store
 app.post(API_STORE, store.post);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// app.use(function (req, res, next) {
+//     // console.log(req);
+//     // console.log('hello world');
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World');
+// });
 
 // starting the server
 app.listen(8080, () => {
--- src/store.js
+++ src/store.js
@@ -8,13 +8,27 @@ const store = {
   get(req, res) {
     const stores = db.get('stores').value();
     res.send(stores);
-  }
+  },
 
   post(req, res) {
-    db.get('posts').push(req).write();
-    
-    const stores = db.get('stores').value();
-    res.send(stores);
+    const id = 123; // latest id incremented
+
+    const store = {
+      city: req.body.city,
+      donation: req.body.donation,
+      id: id,
+      image: req.body.imageText,
+      name: req.body.name,
+      status: 0,
+    };
+
+    db.get('stores').push(store).write();
+
+    console.log(JSON.stringify(req.body));
+
+    // const stores = db.get('stores').value();
+    res.send(store);
+    res.end('Hello World');
   }
 };
 
