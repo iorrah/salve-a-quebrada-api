79f2ccf3eb78bf9fbc7024d03f13bc698d078248
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-16 17:34:08 +0200

    Only deliver valid stores via the API

--- src/store.js
+++ src/store.js
@@ -5,24 +5,34 @@ const utils = require('./utils/utils');
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
-const getStores = () => {
-  const stores = db.defaults(utils.defaultDb).get('stores').value();
+const getStoresReference = () => {
+  const reference = db.defaults(utils.defaultDb).get('stores');
+  return reference;
+};
+
+const getAllStores = () => {
+  const stores = getStoresReference().value();
+  return stores;
+};
+
+const getValidStores = () => {
+  const stores = getStoresReference().filter({ status: 1 }).value();
   return stores;
 };
 
 const store = {
   get(req, res) {
-    const stores = getStores();
+    const stores = getValidStores();
     res.send(stores);
   },
 
   post(req, res) {
-    const stores = getStores();
+    const stores = getAllStores();
     const id = utils.getNewStoreId(stores);
     const store = utils.buildStore(req.body, id);
     db.get('stores').push(store).write();
-    res.send(store);
-    res.end(store);
+    res.send();
+    res.end();
   }
 };
 
